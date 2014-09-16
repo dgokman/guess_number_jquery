8331fe25f6f8c1e0e73dd03b2efe587daa96df18
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-16 13:46:48 -0400

    added flow control

--- guess.js
+++ guess.js
@@ -1,11 +1,22 @@
-var number = Math.floor(Math.random()*101)
+var number = Math.floor(Math.random()*101);
+var guesses = 0;
 
 $('form').on('submit', function(event) {
   event.preventDefault();
 
-  var guessNum = $('#guess').val();
+  var guess = $('#guess').val();
 
-  alert('You typed in ' + guessNum);
+
+  if (parseInt(guess) === number) {
+    console.log("Congratulations, you've guessed the number in " + guesses + " guesses!");
+    guesses = 0;
+  } else if (parseInt(guess) < number) {
+    console.log(guess + ' was too low, try again...');
+    guesses++;
+  } else {
+    console.log(guess + ' was too high, try again...')
+    guesses++
+  }
 });
 
 
