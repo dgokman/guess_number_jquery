70f19a8e3b521e4d16354f0ed6456bf3573af59b
Dennis Gokman <dennis.gokman@gmail.com>
2014-09-16 15:10:38 -0400

    added styling

--- guess.js
+++ guess.js
@@ -7,22 +7,22 @@ $('form#game').on('submit', function(event) {
   var guessNum = $('#guess').val();
 
   if (parseInt(guessNum) >= 0 && parseInt(guessNum) <= 100) {
-  $('body').append('<li>' + guessNum + '</li>');
+    $('body').append('<li>' + guessNum + '</li>');
   }
 
   if (parseInt(guessNum) === number) {
     guesses++;
-    $('p').html("Congratulations, you've guessed the number in " + guesses + " guesses!");
-    $('form#game').remove();
-    $('body').append('<br><form id="restart"><input type="submit" value="play again!"/></form>');
+      $('p').html("Congratulations, you've guessed the number in " + guesses + " guesses!");
+      $('form#game').remove();
+      $('body').append('<br><form id="restart"><input type="submit" value="play again!"/></form>');
   } else if (parseInt(guessNum) < number && parseInt(guessNum) >= 0) {
-    $('p').html(guessNum + ' was too low, try again...');
-    guesses++;
+      $('p').html(guessNum + ' was too low, try again...');
+      guesses++;
   } else if (parseInt(guessNum) > number && parseInt(guessNum) <= 100) {
-    $('p').html(guessNum + ' was too high, try again...');
-    guesses++;
-  } else {
-    $('p').html('Invalid input, must enter a number between 0 and 100');
+      $('p').html(guessNum + ' was too high, try again...');
+      guesses++;
+  }   else {
+      $('p').html('Invalid input, must enter a number between 0 and 100');
   }
 
 });
